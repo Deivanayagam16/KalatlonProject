5d8810643703f8e6009b5c513b39ab161fe3c73a
deivanayagam16 <deivajeyamit@gmail.com>
2020-08-20 14:45:11 +0530

    Third commit

---
+++ Object Repository/WorkShiftManagement/DeleteWorkShift/Page_OrangeHRM/ShiftDetail_Table.rs
@@ -0,0 +1,262 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ShiftDetail_Table</name>
+   <tag></tag>
+   <elementGuidId>d39b329c-ef12-4154-89f4-8470110e825d</elementGuidId>
+   <imagePath></imagePath>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//table[@id='resultTable']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@name = 'frmList_ohrmListComponent' and @id = 'frmList_ohrmListComponent' and (text() = '
+                
+           
+        
+
+        
+
+
+   
+
+         
+
+        
+
+
+         
+        
+
+        
+            
+            
+                
+        
+        
+            
+                                
+                
+Shift Name
+From
+To
+Hours Per Day
+            
+                            
+
+            
+                                            
+                                                    General
+                                                    08:00
+                                                    17:00
+                                                    9.00
+                                            
+                                            
+                                                    Twilight
+                                                    14:00
+                                                    23:00
+                                                    9.00
+                                            
+                                
+                            
+         
+                ' or . = '
+                
+           
+        
+
+        
+
+
+   
+
+         
+
+        
+
+
+         
+        
+
+        
+            
+            
+                
+        
+        
+            
+                                
+                
+Shift Name
+From
+To
+Hours Per Day
+            
+                            
+
+            
+                                            
+                                                    General
+                                                    08:00
+                                                    17:00
+                                                    9.00
+                                            
+                                            
+                                                    Twilight
+                                                    14:00
+                                                    23:00
+                                                    9.00
+                                            
+                                
+                            
+         
+                ')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#frmList_ohrmListComponent</value>
+      </entry>
+      <entry>
+         <key>IMAGE</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>form</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>method</name>
+      <type>Main</type>
+      <value>post</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>action</name>
+      <type>Main</type>
+      <value>/index.php/admin/deleteWorkShifts</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>frmList_ohrmListComponent</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>frmList_ohrmListComponent</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                
+           
+        
+
+        
+
+
+   
+
+         
+
+        
+
+
+         
+        
+
+        
+            
+            
+                
+        
+        
+            
+                                
+                
+Shift Name
+From
+To
+Hours Per Day
+            
+                            
+
+            
+                                            
+                                                    General
+                                                    08:00
+                                                    17:00
+                                                    9.00
+                                            
+                                            
+                                                    Twilight
+                                                    14:00
+                                                    23:00
+                                                    9.00
+                                            
+                                
+                            
+         
+                </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;frmList_ohrmListComponent&quot;)</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//form[@id='frmList_ohrmListComponent']</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='search-results']/div[2]/form</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Work Shifts'])[2]/following::form[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='*'])[3]/following::form[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div[2]/form</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/WorkShiftManagement/Page_OrangeHRM/select_AvailableEmployees.rs
@@ -0,0 +1,156 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_AvailableEmployees</name>
+   <tag></tag>
+   <elementGuidId>cf4fc4c2-6bdd-4c49-8a7f-68643db411bb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//select[@id='workShift_availableEmp']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#workShift_availableEmp</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>select</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>workShift[availableEmp][]</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>selectMany</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>size</name>
+      <type>Main</type>
+      <value>10</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>multiple</name>
+      <type>Main</type>
+      <value>multiple</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>workShift_availableEmp</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Test 1Linda AndersonAaaa Bbb CccAaaa Bbb CccAaaa Bbb CccAaaa Bbb CccAaaa Bbb CccAaaa Bbb CccAaaa Bbb CccAaaa Bbb CccAaaa Bbb CccAaaa Bbb CccAaaa Bbb CccAaaa Bbb CccAaaa Bbb CccAaaa Bbb CccAndrew CdsAndrew CfzyppuhAndrew CjqbqwyaAndrew ClbgkrakAndrew CpxzmkdxAndrew CqtmuxnfRobert Craigg dDibyesh123456 DashSteven EdwardsThomas FlemingHannah FloresAbc Def GhiFiona GraceAmar HaRussel HamiltonSwapnil Dhananjay KambleJasmine MorganJohn Smithtest1 user1test1 user1Hello world</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;workShift_availableEmp&quot;)</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//select[@id='workShift_availableEmp']</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='frmWorkShift']/fieldset/ol/table/tbody/tr[2]/td/select</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Assigned Employees'])[1]/following::select[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Available Employees'])[1]/following::select[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='*'])[3]/preceding::select[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//td/select</value>
+   </webElementXpaths>
+</WebElementEntity>
